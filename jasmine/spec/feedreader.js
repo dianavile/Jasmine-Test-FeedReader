cdaf92e46c8874ba1fe9d89532d80762b4ec0504
@dianavile <diana.vile@gmail.com>
2018-05-29 12:52:33 +0200

    Feat:  check name allFeeds is defined &not empty
    Test loops through each feed in the allFeeds[] object: 
    -  it has a name defined	
    - and the name is not empty.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -41,6 +41,11 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has an name defined and not empty', function() {
+           for(feed of allFeeds) {
+            expect(feed.name).toBeDefined();
+            expect(allFeeds.name).not.toBe('');
+        };    
     });
 
 
