5cc05c8a3680de0e9dc3b3f22e42ef35dbfa2db1
@dianavile <diana.vile@gmail.com>
2018-05-31 09:40:10 +0200

    Fix: .hasClass() is no function
    I have read the Documentation about .hasClass here: https://api.jquery.com/hasClass/
    And decided to change the .hasClass() back to former .classList() method.
    Reason: Jasmine gives error " .hasClass() is no function". Also, I prefer to write all in plain JavaScript, instead of mixing it up with jQuery.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -50,7 +50,7 @@ $(function() {
          * This test checks if menu is present in <body> by default to hide menu.
          */
         it('is hidden by default', function() {
-            expect(body.hasClass('menu-hidden')).toBe(true);
+            expect(body.classList.contains('menu-hidden')).toBe(true);
         }); 
     /* Test 5: Test to check if menu changes visibility, when menu icon is clicked. 
          * Includes two expectations: menu displays when menu icon is clicked
@@ -60,10 +60,10 @@ $(function() {
             let menuIcon = document.getElementsByClassName('menu-icon-link')[0];
 
             menuIcon.click();
-            expect(body.hasClass('menu-hidden')).toBe(false);
+            expect(body.classList.contains('menu-hidden')).toBe(false);
 
             menuIcon.click();
-            expect(body.hasClass('menu-hidden')).toBe(true);
+            expect(body.classList.contains('menu-hidden')).toBe(true);
         });    
     });
 
